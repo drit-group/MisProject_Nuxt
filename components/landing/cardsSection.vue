fe1801d3e82bd4b197040df3caa0e13267fd3d89
lorem10 <momenei.alireza1382.15@gmail.com>
2022-02-21 22:21:40 +0330

    Header and Cards component is ready

---
+++ components/landing/cardsSection.vue
@@ -0,0 +1,56 @@
+<template>
+  <b-row>
+    <b-col v-for="(card , index) in cards" :key="index">
+      <b-card border-variant="0" bg-variant="transparent" align="center">
+        <b-img width="66" :src="`images/landing/card${index+1}.png`"/>
+        <b-card-title class="my-4 text-danger h3">
+          {{ card.title }}
+        </b-card-title>
+        <b-card-text>
+          <p>
+            {{ card.text }}
+          </p>
+        </b-card-text>
+      </b-card>
+    </b-col>
+  </b-row>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: "CardsSection",
+  data() {
+    return {
+      cards: [
+        {
+          title: 'آموزش ها',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint voluptatibus ut similique natus, sapiente dolores harum fugiat minima nihil ducimus. Sunt et animi sit officia nobis ut inventore impedit. Accusamus.'
+        },
+        {
+          title: 'اعضا',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint voluptatibus ut similique natus, sapiente dolores harum fugiat minima nihil ducimus. Sunt et animi sit officia nobis ut inventore impedit. Accusamus.'
+        },
+        {
+          title: 'پروژه ها',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint voluptatibus ut similique natus, sapiente dolores harum fugiat minima nihil ducimus. Sunt et animi sit officia nobis ut inventore impedit. Accusamus.'
+        },
+        {
+          title: 'کارگروه ها',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint voluptatibus ut similique natus, sapiente dolores harum fugiat minima nihil ducimus. Sunt et animi sit officia nobis ut inventore impedit. Accusamus.'
+        }
+      ]
+    }
+  }
+})
+</script>
+
+<style scoped>
+.card:hover {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+  transition: 1s;
+}
+</style>
---
+++ components/landing/header.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-row align-v="center">
+    <b-col md="6" order-md="2">
+      <b-img src="images/landing/header.png" fluid/>
+    </b-col>
+    <b-col md="6" order-md="1">
+      <h2 class="dispaly-4 mb-4"> انجمن برنامه نویسان <span class="text-danger">دکتر آیتی </span></h2>
+      <p class="lead text-muted ">
+        Lorem ipsum dolor sit amet consecteturequuntur! similique ut in hic ducimus error sint nostrum.
+      </p>
+      <button class="btn btn-danger ">ورود</button>
+    </b-col>
+  </b-row>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: "HeaderComp"
+})
+</script>
+
+<style scoped>
+
+</style>
