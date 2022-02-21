1648ca338e3fdac2fa91b82f014fee3c1b4e3f7d
lorem10 <momenei.alireza1382.15@gmail.com>
2022-02-21 22:54:22 +0330

    AboutUs component is ready

---
+++ components/landing/aboutUs.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-row align-v="center">
+    <b-col md="6">
+      <b-img src="images/landing/aboutUs.png" fluid/>
+    </b-col>
+    <b-col md="6">
+      <h2 class="dispaly-4 mb-4"><span class="text-primary">دکتر آیتی </span> چیست ؟؟؟ </h2>
+      <p class="lead text-muted">
+        Lorem ipsum dolor sit amet consecteturequuntur! similique ut in hic ducimus error sint nostrum.
+      </p>
+      <b-btn variant="primary" to="/">
+        <b-icon-chevron-right font-scale="1.3"/>
+        <span>
+          بیشتر بخوانید
+        </span>
+      </b-btn>
+    </b-col>
+  </b-row>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue'
+
+export default Vue.extend( {
+  name: "AboutUs",
+})
+</script>
+
+<style scoped>
+
+</style>
