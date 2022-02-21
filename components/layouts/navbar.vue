7cd428a6f4ba13957c015f0a51eed910bc45fb5b
lorem10 <momenei.alireza1382.15@gmail.com>
2022-02-21 18:59:57 +0330

    navbar component is ready

--- components/layouts/navbar.vue
+++ components/layouts/navbar.vue
@@ -1,17 +1,46 @@
 <template>
-  <header>
-    header
-  </header>
+  <b-navbar sticky toggleable="lg" type="light" class="shadow py-3" variant="white">
+    <b-container>
+      <b-navbar-brand>
+        <b-link to="/" class="text-dark text-decoration-none">
+          Dr.IT
+        </b-link>
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="mx-auto">
+          <b-nav-item v-for="(item , index) in navData" :key="index">
+            <b-link :to="item.path" class="text-dark text-decoration-none">
+              {{ item.name }}
+            </b-link>
+          </b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav>
+          <b-btn variant="primary">ورود</b-btn>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: "NavbarComp"
+  name: 'NavbarComp',
+  data() {
+    return {
+      navData: [
+        {name: 'خانه', path: '/'},
+        {name: 'برنامه ها', path: '/'},
+        {name: 'دکتر آیتی', path: '/'},
+        {name: 'درباره ما', path: '/'},
+      ]
+    }
+  }
 })
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
