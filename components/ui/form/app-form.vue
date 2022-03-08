14b5a3de3a50a6afb009eb775739f34bbf3b52be
lorem10 <momenei.alireza1382.15@gmail.com>
2022-03-08 14:44:14 +0330

    add : basic form components

---
+++ components/ui/form/app-form.vue
@@ -0,0 +1,27 @@
+<template>
+  <validation-observer v-slot="{handleSubmit}">
+    <b-form @submit.prevent="handleSubmit(submitForm)">
+      <slot></slot>
+    </b-form>
+  </validation-observer>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {ValidationObserver} from "vee-validate";
+
+export default Vue.extend({
+  name: "AppForm",
+  components: {ValidationObserver},
+  props: {
+    submitForm: {
+      required: true,
+      type: Function
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/ui/form/app-input.vue
@@ -0,0 +1,42 @@
+<template>
+  <validation-provider v-slot="{errors}" :rules="inputOption.rules" :name="inputOption.label">
+    <b-form-group :label="inputOption.label" :label-for="inputOption.id">
+      <b-form-input :id="inputOption.id" v-model="inputValue" class="rounded-0"  :aria-describedby="`${inputOption.id}_error`" :type="inputOption.type" :size="inputOption.size"/>
+      <b-form-text :id="`${inputOption.id}_error`" text-variant="danger">
+        {{ errors[0] }}
+      </b-form-text>
+    </b-form-group>
+  </validation-provider>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {ValidationProvider} from "vee-validate";
+
+type InputpropsObjext = {
+  label: string,
+  id: string,
+  size: string,
+  type: string,
+  rules: string,
+}
+
+export default Vue.extend({
+  name: 'AppInput',
+  components: {ValidationProvider},
+  props: {
+    inputOption: {
+      required: true,
+      type: Object as () => InputpropsObjext
+    },
+    inputValue: {
+      required: true,
+      type: String
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
