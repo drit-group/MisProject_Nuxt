7a09c050a83b2914a2392a994955a6ddba77ef6e
lorem10 <momenei.alireza1382.15@gmail.com>
2022-03-08 14:48:54 +0330

    add : auth layout

---
+++ layouts/auth.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-container fluid>
+    <Nuxt/>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "AuthLayout"
+}
+</script>
+
+<style>
+.h-100-vh {
+  height: 100vh;
+}
+</style>
