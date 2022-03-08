01cda4c9f0e3008ddf6ccea2016ac7473c59da05
lorem10 <momenei.alireza1382.15@gmail.com>
2022-03-08 14:45:28 +0330

    add : login and register is ready

--- components/auth/login.vue
+++ components/auth/login.vue
@@ -1,21 +1,57 @@
 <template>
-  <div>
-    login
-    <nuxt-link :to="{name:'auth-componentName' , params:{componentName : 'register'}}">
-      register
-    </nuxt-link>
+  <div class="form-style m-2">
+    <h1>خوش آمدید</h1>
+    <p class="w-100 pb-3">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان </p>
+    <app-form :submit-form="submit">
+      <app-input :input-option="inputEmailOption" :input-value="inputEmailValue"/>
+      <app-input :input-option="inputPassOption" :input-value="inputPassValue"/>
+      <b-btn type="submit" variant="primary" size="lg" block class="mt-4">
+        ارسال
+      </b-btn>
+    </app-form>
+    <p class="mt-3">
+      اگر حساب کاربری ندارید
+      <nuxt-link :to="{name:'auth-componentName' , params:{componentName : 'register'}}">
+        ثبت نام
+      </nuxt-link>
+      کنید
+    </p>
   </div>
 </template>
+
 
 <script lang="ts">
 
 import Vue from 'vue'
+import appInput from "~/components/ui/form/app-input.vue";
+import appForm from "~/components/ui/form/app-form.vue";
 
 export default Vue.extend({
   name: "LoginComp",
+  components: {appInput, appForm},
+  data() {
+    return {
+      inputEmailValue: '',
+      inputPassValue: '',
+      inputEmailOption: {
+        label: 'کدملی',
+        id: 'inputNationalCode',
+        size: 'lg',
+        type: 'number',
+        rules: 'required|length:10|numeric'
+      },
+      inputPassOption: {
+        label: 'رمز عبور',
+        id: 'inputPass',
+        size: 'lg',
+        type: 'password',
+        rules: 'required|min:8|max:30'
+      }
+    }
+  },
   methods: {
-    changeComp(): void {
-      this.$emit('changeComp', 'register')
+    submit() {
+      alert('submitted')
     }
   }
 })
--- components/auth/register.vue
+++ components/auth/register.vue
@@ -1,18 +1,87 @@
 <template>
-  <div>
-    register
-    <nuxt-link :to="{name:'auth-componentName' , params:{componentName : 'login'}}">
-      login
-    </nuxt-link>
+  <div class="form-style m-2">
+    <h1 >ثبت نام</h1>
+    <p class="w-100 pb-3">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان </p>
+    <app-form :submit-form="submit">
+      <div class="d-flex">
+        <app-input :input-option="inputFNameOption" class="mr-2" :input-value="inputFNameValue"/>
+        <app-input :input-option="inputLNameOption" class="ml-2" :input-value="inputLNameValue"/>
+      </div>
+      <app-input :input-option="inputNationalCodeOption" :input-value="inputNationalCodeValue"/>
+      <app-input :input-option="inputEmailOption" :input-value="inputEmailValue"/>
+      <app-input :input-option="inputPassOption" :input-value="inputPassValue"/>
+      <b-btn type="submit" variant="primary" size="lg" block class="mt-4">
+        ارسال
+      </b-btn>
+    </app-form>
+    <p class="mt-3">
+      اگر حساب کاربری ندارید
+      <nuxt-link :to="{name:'auth-componentName' , params:{componentName : 'login'}}">
+        login
+      </nuxt-link>
+      کنید
+    </p>
   </div>
 </template>
 
 <script lang="ts">
 
 import Vue from 'vue'
+import appInput from "~/components/ui/form/app-input.vue";
+import appForm from "~/components/ui/form/app-form.vue";
 
 export default Vue.extend({
   name: "RegisterComp",
+  components: {appInput, appForm},
+  data() {
+    return {
+      inputNationalCodeValue: '',
+      inputPassValue: '',
+      inputEmailValue:'',
+      inputFNameValue:'',
+      inputLNameValue:'',
+      inputNationalCodeOption: {
+        label: 'کدملی',
+        id: 'inputNationalCode',
+        size: 'lg',
+        type: 'number',
+        rules: 'required|length:10|numeric'
+      },
+      inputEmailOption: {
+        label: 'ایمیل',
+        id: 'inputEmail',
+        size: 'lg',
+        type: 'email',
+        rules: 'required|email'
+      },
+      inputFNameOption: {
+        label: 'نام',
+        id: 'inputFName',
+        size: 'lg',
+        type: 'text',
+        rules: 'required|alpha'
+      },
+      inputLNameOption: {
+        label: 'نام خانوادگی',
+        id: 'inputLName',
+        size: 'lg',
+        type: 'text',
+        rules: 'required|alpha'
+      },
+      inputPassOption: {
+        label: 'رمز عبور',
+        id: 'inputPass',
+        size: 'lg',
+        type: 'password',
+        rules: 'required|min:8|max:30'
+      }
+    }
+  },
+  methods: {
+    submit() {
+      alert('submitted')
+    }
+  }
 })
 </script>
 
